6fef12f7e69fe0791d54f348954e39d90e720acd
Carman Fu <futarimiti@163.com>
2024-10-16 06:25:50 +0100

    fix a few things

--- lua/thecool/packer.lua
+++ lua/thecool/packer.lua
@@ -1,50 +1,64 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- bootstrap
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+-- list of plugins
+return require("packer").startup(function(use)
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
 
-    use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-    }
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
-    use ({
-    'folke/tokyonight.nvim',
-    as = 'tokyonight',
-    config = function ()
-        vim.cmd('colorscheme tokyonight')
-    end
-    })
+  use({
+    "folke/tokyonight.nvim",
+    as = "tokyonight",
+    config = function()
+      vim.cmd("colorscheme tokyonight")
+    end,
+  })
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-    use('mbbill/undotree')
+  use("mbbill/undotree")
 
-    use {
-    'VonHeikemen/lsp-zero.nvim',
+  use({
+    "VonHeikemen/lsp-zero.nvim",
     requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
+  })
 
-    use('mfussenegger/nvim-jdtls')
+  use("mfussenegger/nvim-jdtls")
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then require('packer').sync() end
 end)
